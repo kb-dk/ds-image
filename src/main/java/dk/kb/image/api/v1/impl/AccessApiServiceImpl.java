6f5d79dc6296116ddd41141d187ae994280fb5c8
Victor Harbo Johnston <vhol@kb.dk>
2022-08-29 15:10:58 +0200

    Adds mimetype for dzi response

--- src/main/java/dk/kb/image/api/v1/impl/AccessApiServiceImpl.java
+++ src/main/java/dk/kb/image/api/v1/impl/AccessApiServiceImpl.java
@@ -44,8 +44,8 @@ public class AccessApiServiceImpl extends ImplBase implements AccessApi {
         // TODO: Implement...
 
         try {
+            httpServletResponse.setContentType(getMIME("json"))
             setFilename(new File(imageid).getName() + ".dzi", false, false);
-
             return IIPFacade.getInstance().getDeepzoomDZI(
                     uriInfo.getRequestUri(),
                     imageid);
