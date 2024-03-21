389fe8e027807c2bc8b080e8393ff9e7214caa28
Thomas Egense <teg@kb.dk>
2024-03-21 13:26:06 +0100

    review changes. 1 ignore file

--- src/test/java/dk/kb/image/kaltura/KalturaApiIntegrationTest.java
+++ src/test/java/dk/kb/image/kaltura/KalturaApiIntegrationTest.java
@@ -4,12 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import dk.kb.image.config.ServiceConfig;
+import dk.kb.image.util.DsImageClientTest;
 import dk.kb.image.util.KalturaUtil;
 
 /**
- * Unittest that will call the API api search method. Search for a local refenceId to get the Kaltura internal id for the record.
+ * Unittest that will call the API search method. Search for a local refenceId to get the Kaltura internal id for the record.
  * This API call fails often when running in a web-container (Tomcat/Jetty) and we do not know why. 
  * It fails both when reusing the same session or always creating a new session.
  * But it works when called from a minimal maven project with only Kaltura Dependency
@@ -22,7 +25,7 @@ import dk.kb.image.util.KalturaUtil;
 @Tag("integration")
 public class KalturaApiIntegrationTest {
 
-    
+    private static final Logger log = LoggerFactory.getLogger(KalturaApiIntegrationTest.class);
     @Test
     public void callKalturaApi() throws Exception{
         ServiceConfig.getInstance().initialize("conf/ds-image-kaltura-integration-test.yaml"); //Load Kaltura API access properties. This file is in aegis and not the project.
@@ -33,14 +36,12 @@ public class KalturaApiIntegrationTest {
                 
         int success=0;
         for (int i = 0;i<20;i++) {
-            DsKalturaClient client = KalturaUtil.getClientInstance(); //This will reuse the session as it is configured in KalturaUtil.            
-            
+            DsKalturaClient client = KalturaUtil.getClientInstance(); //This will reuse the session as it is configured in KalturaUtil.                  
             String kalturaId = client.getKulturaInternalId(referenceId);
-            assertEquals(kalturaInternallId, kalturaId,"API was reproduced after number of calls:"+success);
-            success++;
-            Thread.sleep(1000L);
-                System.out.println(kalturaId);
-            
+            assertEquals(kalturaInternallId, kalturaId,"API error was reproduced after "+success+" number of calls");
+            log.debug("API returned internal Kaltura id:"+kalturaId);
+            success++;            
+            Thread.sleep(1000L);                        
         }
         
          
