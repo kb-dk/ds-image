963d450da70d185e98cc1450bc761fe2bbce9d4c
Victor Harbo Johnston <vhol@kb.dk>
2024-04-08 12:29:12 +0200

    Update OpenApiResource

--- src/main/java/dk/kb/image/webservice/Application_v1.java
+++ src/main/java/dk/kb/image/webservice/Application_v1.java
@@ -8,12 +8,17 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.fasterxml.jackson.jaxrs.xml.JacksonXMLProvider;
 import dk.kb.image.api.v1.impl.AccessApiServiceImpl;
 import dk.kb.image.api.v1.impl.ServiceApiServiceImpl;
+import dk.kb.image.config.ServiceConfig;
+import dk.kb.util.webservice.OpenApiResource;
+
 
 
 public class Application_v1 extends javax.ws.rs.core.Application {
 
     @Override
     public Set<Class<?>> getClasses() {
+        OpenApiResource.setConfig(ServiceConfig.getConfig());
+
         return new HashSet<>(Arrays.asList(
                 JacksonJsonProvider.class,
                 JacksonXMLProvider.class,
